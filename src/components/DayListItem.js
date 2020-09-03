6cb5e40370158ba5a7e6ae1d99fc539f3db11779
Christy Lum <christy.lum121@gmail.com>
2020-09-03 16:48:45 -0700

    Implemented day list and day list item components

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,24 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classnames from "classnames";
 
 export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots == 0
+  });
+
+  const formatSpots = (spots) => {
+    let string = "";
+    if (spots == 0) return (string = "no spots remaining");
+    if (spots == 1) return (string = "1 spot remaining");
+    return (string = props.spots + " spots remaining");
+  };
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={dayClass} onClick={props.setDay}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
