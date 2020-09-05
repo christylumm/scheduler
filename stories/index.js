86ab0c109b1dc344dd43d5dd043cf233d697724b
Christy Lum <christy.lum121@gmail.com>
2020-09-04 17:24:44 -0700

    Implemented InterviwerListItem.js

--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,10 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem.js";
 import DayList from "components/DayList.js";
+import InterviewerListItem from "components/InterviewerListItem.js";
+
+
+// BUTTON MODULE -------------------------------
 
 storiesOf("Button", module)
   .addParameters({
@@ -25,7 +29,8 @@ storiesOf("Button", module)
     </Button>
   ));
 
-  storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
+// DAYLISTITEM MODULE -------------------------------
+storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
   .addParameters({
     // Provides the default background color for our component
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -39,7 +44,9 @@ storiesOf("Button", module)
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> 
   ));
 
-  const days = [
+// DAYS ARRAY -------------------------------
+
+const days = [
     {
       id: 1,
       name: "Monday",
@@ -57,13 +64,49 @@ storiesOf("Button", module)
     },
   ];
   
-  storiesOf("DayList", module)
-    .addParameters({
-      backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
-    })
-    .add("Monday", () => (
-      <DayList days={days} day={"Monday"} setDay={action("setDay")} />
-    ))
-    .add("Tuesday", () => (
-      <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
-    ));
+// DAYLIST MODULE -------------------------------
+storiesOf("DayList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Monday", () => (
+    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+  ))
+  .add("Tuesday", () => (
+    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+  ));
+
+// INTERVIEWERLIST MODULE ------------------------
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
