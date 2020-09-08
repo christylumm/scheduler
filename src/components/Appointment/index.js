e0a800603c5375752ec8fbd87f63921e1fbfc136
Christy Lum <christy.lum121@gmail.com>
2020-09-08 13:53:57 -0700

    added transition between empty, create, and show modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,19 +7,39 @@ import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
+import Form from "./Form";
 
 //VARIABLES TO SWITCH MODES ----------------------------------
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
-  const fragment = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />;
-  // const fragment = <Empty />;
+  //const fragment = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />;
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {fragment}
+
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+
+      {mode === CREATE && (
+        <Form
+          onCancel={() => back()}
+          interviewers={[]}
+        />
+      )}
+
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
     </article>
   );
 }
